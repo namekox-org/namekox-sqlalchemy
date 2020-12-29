008028f83fa3e3302c9393237e53e559f4e37886
李满满 <limanman@corp.netease.com>
2020-12-29 11:40:37 +0800

    :underage:

--- namekox_sqlalchemy/core/dependencies/__init__.py
+++ namekox_sqlalchemy/core/dependencies/__init__.py
@@ -4,8 +4,8 @@
 
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from namekox_core.core.friendly import AsLazyProperty
+from sqlalchemy.orm import sessionmaker, scoped_session
 from namekox_core.core.service.dependency import Dependency
 from namekox_sqlalchemy.constants import DATABASE_CONFIG_KEY
 
@@ -13,9 +13,9 @@ from namekox_sqlalchemy.constants import DATABASE_CONFIG_KEY
 class Database(Dependency):
     def __init__(self, dbname, dbbase, session_wrapper=None, engine_options=None, session_options=None):
         self.engine = None
-        self.session = None
         self.dbname = dbname
         self.dbbase = dbbase
+        self.session_map = {}
         self.session_cls = None
         self.session_wrapper = session_wrapper
         self.engine_options = engine_options or {}
@@ -29,7 +29,7 @@ class Database(Dependency):
     def setup(self):
         duri = self.uris[self.dbname].format(dbname=self.dbbase.__name__)
         self.engine = create_engine(duri, **self.engine_options)
-        self.session_cls = sessionmaker(bind=self.engine, **self.session_options)
+        self.session_cls = scoped_session(sessionmaker(bind=self.engine, **self.session_options))
         self.session_cls = self.session_wrapper(self.session_cls) if self.session_wrapper else self.session_cls
 
     def stop(self):
@@ -37,8 +37,9 @@ class Database(Dependency):
         del self.engine
 
     def get_instance(self, context):
-        self.session = self.session_cls()
-        return self.session
+        self.session_map[context.call_id] = self.session_cls()
+        return self.session_map[context.call_id]
 
     def worker_teardown(self, context):
-        self.session.close()
+        session = self.session_map.pop(context.call_id, None)
+        session and session.close()
