1b09c2e10be36066ed9c84b16105f52d275b2956
李满满 <limanman@corp.netease.com>
2021-02-23 21:38:48 +0800

    :underage:

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #! -*- coding: utf-8 -*-
-
+#
 # author: forcemain@163.com
 
+from __future__ import unicode_literals
 
 import os
 
@@ -15,7 +16,7 @@ readme = open(os.path.join(curdir, 'READIT.md'), 'rb', 'utf-8').read()
 
 setup(
     name='namekox-sqlalchemy',
-    version='0.0.10',
+    version='0.0.11',
     description='namekox sqlalchemy',
     long_description_content_type='text/markdown',
     long_description=readme,
